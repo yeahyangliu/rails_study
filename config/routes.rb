30a438ba50c4651dc637c35aa8f65bf40a9aac30
YANG Liu <yeah_yangliu@163.com>
2014-12-30 12:21:00 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'application#hello'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
