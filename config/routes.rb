ea4117dfb7900cabbd50eecf4138774899011604
YANG Liu <yeah_yangliu@163.com>
2015-01-04 16:14:04 +0800

    add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :microposts
 
   resources :users
@@ -6,7 +12,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
